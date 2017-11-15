e79c376526ed9e085d1cc95b3d28095167aa40fe
Daniel Bautista Hernández <Danielbahe@gmail.com>
2017-11-15 20:39:19 +0100

    master: model merge

--- EventService/EventService/Models/Person.cs
+++ EventService/EventService/Models/Person.cs
@@ -4,11 +4,13 @@ namespace EventService.Models
 {
     public class Person
     {
-        public int Id { get; set; }
         public int CollaId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string SurName { get; set; }
         public string Alias { get; set; }
+        public int Height { get; set; }
+        public float Weight { get; set; }
         public DateTime Birth { get; set; }
     }
 }
